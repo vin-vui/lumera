34223e366b8fd82eab2bffcdf4c7ef61d776aca5
vinvui <admin@vinvui.com>
2023-04-21 08:40:49 +0200

    Remove errors pages for test

--- routes/web.php
+++ routes/web.php
@@ -51,17 +51,17 @@ Route::get('/404', function () {
     return view('errors.404');
 });
 
-Route::get('/500', function () {
-    return view('errors.500');
-});
+// Route::get('/500', function () {
+//     return view('errors.500');
+// });
 
 // Route::get('/503', function () {
 //     return view('errors.503');
 // });
 
-Route::get('/401', function () {
-    return view('errors.401');
-});
+// Route::get('/401', function () {
+//     return view('errors.401');
+// });
 
 Route::middleware([
     'auth:sanctum',
