5de071ceb8141f9c4355ad806f589a17935b37ec
vinvui <vincent.vuillemin@gmail.com>
2023-05-05 18:22:17 +0200

    Hotfix #5

--- config/filesystems.php
+++ config/filesystems.php
@@ -44,18 +44,10 @@ return [
             'throw' => false,
         ],
 
-        // 'uploads' => [
-        //     'driver' => 'local',
-        //     'root' => public_path('uploads'),
-        //     'url' => env('APP_URL').'/uploads',
-        //     'visibility' => 'public',
-        //     'throw' => false,
-        // ],
-
         'uploads' => [
             'driver' => 'local',
-            'root' => storage_path('app/public/uploads'),
-            'url' => env('APP_URL').'/storage/uploads',
+            'root' => public_path('uploads'),
+            'url' => env('APP_URL').'/uploads',
             'visibility' => 'public',
             'throw' => false,
         ],
